079e795d165b5e2383300fa56ece48f8c36df372
Albi <albikrasniqi31@gmail.com>
2022-05-20 17:43:48 +0200

    add updatePost

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,5 +1,6 @@
 const Post = require('../models/postModel');
 const Category = require('../models/categoryModel');
+const path = require('path');
 
 exports.admin = (req, res) => {
   res.render('admin/index');
@@ -36,6 +37,34 @@ exports.posts = (req, res) => {
     .then((posts) => {
       res.render('admin/posts', { posts: posts });
     });
+};
+
+exports.getEditPost = (req, res) => {
+  Post.findOne({ _id: req.params.id }).then((post) => {
+    Category.find({}).then((categories) => {
+      res.render('admin/editpost', { post: post, categories: categories });
+    });
+  });
+};
+
+exports.updatePost = (req, res) => {
+  let post_image = req.files.post_image;
+
+  post_image.mv(
+    path.resolve(__dirname, '../public/img/postimages', post_image.name)
+  );
+
+  Post.findOne({ _id: req.params.id }).then((post) => {
+    post.title = req.body.title;
+    post.content = req.body.content;
+    post.date = req.body.date;
+    post.category = req.body.category;
+    post.post_image = `/img/postimages/${post_image.name}`;
+
+    post.save().then((post) => {
+      res.redirect('/admin/posts');
+    });
+  });
 };
 
 exports.deletePost = (req, res) => {
--- routes/admin/indexRoutes.js
+++ routes/admin/indexRoutes.js
@@ -13,6 +13,10 @@ router.delete('/categories/:id', adminController.deleteCategory);
 
 router.get('/posts', adminController.posts);
 
+router.get('/posts/edit/:id', adminController.getEditPost);
+
+router.put('/posts/:id', adminController.updatePost);
+
 router.delete('/posts/:id', adminController.deletePost);
 
 module.exports = router;
