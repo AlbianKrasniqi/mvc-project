9dc90d69c299b85aed622ef8ccc03b9ececddc02
Albi <albikrasniqi31@gmail.com>
2022-06-06 13:40:46 +0200

    backend pagination

--- controllers/postController.js
+++ controllers/postController.js
@@ -2,7 +2,6 @@ const Post = require('../models/postModel');
 const Category = require('../models/categoryModel');
 const User = require('../models/userModel');
 const path = require('path');
-const { router } = require('../app');
 
 exports.addpost = (req, res) => {
   if (!req.session.userId) {
@@ -35,28 +34,40 @@ exports.addposttest = (req, res) => {
 };
 
 exports.getAllPosts = (req, res) => {
+  const postPerPage = 2;
+  const page = req.query.page || 1;
+
   Post.find({})
     .populate({ path: 'author', model: User })
     .sort({ $natural: -1 })
+    .skip(postPerPage * page - postPerPage)
+    .limit(postPerPage)
     .then((posts) => {
-      Category.aggregate([
-        {
-          $lookup: {
-            from: 'posts',
-            localField: '_id',
-            foreignField: 'category',
-            as: 'posts',
+      Post.countDocuments().then((postCount) => {
+        Category.aggregate([
+          {
+            $lookup: {
+              from: 'posts',
+              localField: '_id',
+              foreignField: 'category',
+              as: 'posts',
+            },
           },
-        },
-        {
-          $project: {
-            _id: 1,
-            name: 1,
-            num_of_posts: { $size: '$posts' },
+          {
+            $project: {
+              _id: 1,
+              name: 1,
+              num_of_posts: { $size: '$posts' },
+            },
           },
-        },
-      ]).then((categories) => {
-        res.render('site/blog', { posts: posts, categories: categories });
+        ]).then((categories) => {
+          res.render('site/blog', {
+            posts: posts,
+            categories: categories,
+            current: parseInt(page),
+            pages: Math.ceil(postCount / postPerPage),
+          });
+        });
       });
     });
 };
