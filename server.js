67d15694d77aa50591c90c96370196a084f92cdc
Albi <albikrasniqi31@gmail.com>
2022-06-07 15:33:25 +0200

    some changes

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ mongoose
   });
 
 const port = process.env.PORT || 3000;
-
 const hostname = '127.0.0.1';
+
 app.listen(port, hostname, () => {
   console.log(`Port is running on port: http://${hostname}:${port}/`);
 });
