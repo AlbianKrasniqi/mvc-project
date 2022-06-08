c5ca5ac1874a6cb6a90c9c5777647637b2bb800b
Albi <albikrasniqi31@gmail.com>
2022-06-08 11:15:07 +0200

    fixed mainC

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,5 +1,3 @@
-const Post = require('../models/postModel');
-
 exports.index = (req, res) => {
   console.log(req.session);
   res.render('site/index');
@@ -8,12 +6,6 @@ exports.index = (req, res) => {
 exports.about = (req, res) => {
   res.render('site/about');
 };
-
-// exports.blog = (req, res) => {
-//   Post.find().then((posts) => {
-//     res.render('site/blog', { posts: posts });
-//   });
-// };
 
 exports.contact = (req, res) => {
   res.render('site/contact');
